df55bde90049650917e24c17727fdd1f9d1833d9
Michel3-01 <95318940+Michel3-01@users.noreply.github.com>
2022-01-04 20:32:25 -0200

    criação do arquivo colaborador_dao

--- model/colaborador.py
+++
@@ -1,7 +0,0 @@
-class Colaborador():
-    def __init__(self, id, nome, email):
-        self.id = id
-        self.nome = nome
-        self.email= email
-    def print(self):
-        print(f'{self.id}, {self.nome} ,{self.email}')
--- qt_core.py
+++
@@ -1,5 +0,0 @@
-import sys,os
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5 import uic
