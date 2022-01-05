43dafc5fa71d3819eda5e8c6c9cf7bf05f814915
Michel3-01 <95318940+Michel3-01@users.noreply.github.com>
2022-01-05 20:40:54 -0200

    Alteração no arquivo colaborador_dao

--- model/colaborador_dao.py
+++ model/colaborador_dao.py
@@ -9,14 +9,17 @@ def getColaborador(id):
             return colaborador
 def editar(colaborador):
     for i in range(0,len(lista_de_colaboradores)):
-        if colaborador.id == lista_de_colaboradores[i].id:
+        colaborador_atual = lista_de_colaboradores[i]
+        if colaborador.id == colaborador_atual.id:
             lista_de_colaboradores[i]= colaborador
 
 #Excluir um colaborador.
 def excluir(colaborador):
-    for i in range(0,len(lista_de_colaboradores)):
-        if colaborador.id == lista_de_colaboradores[i].id:
-            lista_de_colaboradores.remove(colaborador)
+    for index in range(0,len(lista_de_colaboradores)):
+        colaborador_atual = lista_de_colaboradores[index]
+        if colaborador.id == colaborador_atual.id:
+            del lista_de_colaboradores[index]
+            return
 #Lista todos os colaboradores.
 def listaTodos():
     for colaborador in lista_de_colaboradores:
