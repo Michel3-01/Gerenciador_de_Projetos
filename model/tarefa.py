31fe96ac619b9fab36ab3a1b5f3a1563ba71e407
Michel3-01 <95318940+Michel3-01@users.noreply.github.com>
2022-01-05 22:16:15 -0200

    Criação da função print.

--- model/tarefa.py
+++ model/tarefa.py
@@ -1,6 +1,8 @@
 class Tarefa():
-    def __init__(self,id,discricao,status):
+    def __init__(self,id,descricao,status):
         self.id = id
-        self.discricao = discricao
+        self.descricao = descricao
         self.status = status
+    def print(self):
+        print(f'{self.id}- {self.descricao}-{self.status}')
         
